dc50d20352db3fdd83081f518eac1a4901f52df7
Arkadiusz Sulborski <arkadiuszsulborski@gmail.com>
2023-01-13 11:53:41 +0100

    added remove button and updated js

--- js/scripts.js
+++ js/scripts.js
@@ -10,28 +10,45 @@
             },
       ];
 
-      const render = () => {
-            let htmlString = "";
-
-            for (const task of tasks) {
-              htmlString += `
-                  <li
-                  ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-                  ${task.content}
-                  </li>
-                  `;
-            };
-            
-            document.querySelector(".js-task").innerHTML = htmlString;
-      };
-
       const addNewTask = (newTaskContent) => {
             tasks.push({
                   content: newTaskContent
             });
 
             render();
+      };
+
+      const removeTask = (index) => {
+            tasks.splice(index, 1)
+            render();
       }
+
+      const render = () => {
+            let htmlString = "";
+
+            for (const task of tasks) {
+              htmlString += `
+                  <li
+                  ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                  >
+                  <button class="js-delete">Usuń</button>
+                  ${task.content}
+                  </li>
+                  `;
+            };
+            
+            document.querySelector(".js-task").innerHTML = htmlString;
+
+            const deleteButtons = document.querySelectorAll(".js-delete");
+
+            deleteButtons.forEach((deleteButton, index) => {
+                  deleteButton.addEventListener("click", () => {
+                       removeTask(index);
+                  });
+            })
+      };
+
+      
 
       const onFormSubmit = (event) => {
             event.preventDefault();
